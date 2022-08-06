28958902acae69207cbfeec44ca297c695e434b1
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2022-08-06 18:02:38 -0300

    Backend project created

---
+++ backend/src/test/java/com/vicente/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicente.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
