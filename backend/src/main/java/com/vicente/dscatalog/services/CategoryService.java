592c51ac6cfa6a7354b220b884acde4b92b6fc84
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2022-08-25 22:17:52 -0300

    Pagination

--- backend/src/main/java/com/vicente/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/vicente/dscatalog/services/CategoryService.java
@@ -1,14 +1,14 @@
 package com.vicente.dscatalog.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,9 +25,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll() {
-		List<Category> list = repository.findAll();
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Category> list = repository.findAll(pageRequest);
+		return list.map(x -> new CategoryDTO(x));
 	}
 
 	@Transactional(readOnly = true)
@@ -35,7 +35,6 @@ public class CategoryService {
 		Optional<Category> obj = repository.findById(id);
 		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
-
 	}
 
 	@Transactional
@@ -58,7 +57,7 @@ public class CategoryService {
 			throw new ResourceNotFoundException("Id not found " + id);
 		}
 	}
-
+	
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
